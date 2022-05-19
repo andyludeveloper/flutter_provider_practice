556ab9fbb0dba483da7d7d8767190e8aa513f80e
Andy <andyludeveloper@gmail.com>
2022-05-19 11:49:28 +0800

    add title for start_page.dart

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/pages/sensors.dart';
 import 'package:flutter_practice/pages/test_page1.dart';
+import 'package:provider/provider.dart';
+
+import '../models/theme_notifier.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Text(
-              "SMI",
-              style: TextStyle(fontSize: 30.0),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const TestPage1()));
-                },
-                child: const Text("Start"))
-          ],
+    return Consumer(builder: (context, ThemeNotifier themeNotifier, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("SMI"),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                "SMI",
+                style: TextStyle(fontSize: 30.0),
+              ),
+              SwitchListTile(
+                  value: themeNotifier.darkTheme,
+                  onChanged: (_) {
+                    themeNotifier.toggleTheme();
+                  }),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const TestPage1()));
+                  },
+                  child: const Text("Start"))
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
