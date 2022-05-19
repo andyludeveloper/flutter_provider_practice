960968ef0dcd158a1e73d2073277cad3092448b2
Andy <andyludeveloper@gmail.com>
2022-05-19 11:23:09 +0800

    Add title for result_page.dart

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -8,20 +8,25 @@ class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: const Text("Result Page"),
+        ),
         body: Consumer<TestResult>(builder: (context, testResult, _) {
           final result = testResult.result;
-          return ListView.builder(itemBuilder: (context, index) {
-            final item = result.keys.elementAt(index);
-            final value = result[item];
-            return ListTile(
-              title: Text(item),
-              trailing: value == true
-                  ? const Icon(Icons.check_circle, color: Colors.lightGreen)
-                  : const Icon(Icons.error_outline_sharp,
-                      color: Colors.redAccent),
-            );
-          }, itemCount: result.keys.length,);
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final item = result.keys.elementAt(index);
+              final value = result[item];
+              return ListTile(
+                title: Text(item),
+                trailing: value == true
+                    ? const Icon(Icons.check_circle, color: Colors.lightGreen)
+                    : const Icon(Icons.error_outline_sharp,
+                        color: Colors.redAccent),
+              );
+            },
+            itemCount: result.keys.length,
+          );
         }));
   }
 }
